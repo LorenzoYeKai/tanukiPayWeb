02165690567aea967d9590a05316413a9f5d22fb
Lorenzo Ye <ye.lorenzo1@gmail.com>
2025-02-01 11:37:00 +0100

    test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col justify-around items-center h-full">
+      <Image
+        src="/2_receive.png"
+        alt="Logo"
+        className="h-full"
+        width={500}
+        height={500}
+      />
+    </div>
+  );
+};
+
+export default page;
